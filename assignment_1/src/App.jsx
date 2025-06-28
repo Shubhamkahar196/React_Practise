4d8744fe7cd87cbcb126e68a49f04a19e4b309a7
Shubham kahar <Shubhamkahar196@gmail.com>
2025-06-28 23:58:41 +0530

    Completeda assignment-1 with tailwind css

--- assignment_1/src/App.jsx
+++ assignment_1/src/App.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const Increament = () =>{
+      setCount(count + 1 );
+  }
+
+  const Decrement = () => {
+    setCount(count - 1);
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+         <div className=' h-screen bg-green-600'>
+          <h1 className='bg-blue-700 h-[40px]'>Counter : {count} </h1>
+          <div className='bg-yellow-500 h-[50px] ' >
+             <button  onClick={Increament} type='button' className='tex-white bg-slate-800 hover:bg-slate-500 focus:ring-4 focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-slate-600 dark:hover:bg-slate-700 focus:outline-none dark:focus:ring-slate-800 '>Increase</button>
+          </div>
+         
+         <div>
+              <button className='tex-white bg-blue-800 hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ' onClick={Decrement}>Decrease</button>
+         </div>
+         
+         </div>
+     
     </>
   )
 }
