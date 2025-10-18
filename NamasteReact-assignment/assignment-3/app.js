dd1ad7dbfebedca1488cc593d46c3d20c1d90eb5
Shubham kahar <Shubhamkahar196@gmail.com>
2025-10-18 10:22:46 +0530

    Completed assignment-3

---
+++ NamasteReact-assignment/assignment-3/app.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import './style.css'
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img src="./icon.png" alt="Logo" className="img-logo" />
+        <input type="text" className="input" placeholder="Type something..." />
+        <img src="./profile.png" alt="" className="img-profile" />
+      </nav>
+    </header>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Header/>)
