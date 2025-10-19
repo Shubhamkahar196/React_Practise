b43598553881e2d37943ac04dfb4494f055e67bd
Shubham kahar <Shubhamkahar196@gmail.com>
2025-10-19 12:41:39 +0530

    understanding useState and added top rated restaurant filter

--- NamasteReact/react-4/src/components/Body.jsx
+++ NamasteReact/react-4/src/components/Body.jsx
@@ -1,18 +1,24 @@
 import resObj from "../utils/mockData"
 import RestaurantCard from "./RestaurantCard"
-
+import { useState } from "react"
 const Body = () =>{
+// local  state variable - super powerful variable
+  const [listRes, SetListRes] = useState(resObj);
+
     return (
          
         <div className="body">
             <div className="search-container">
-              <input className="search" placeholder="type something here.." />
-              <button className="btn">Search</button>
+              {/* <input className="search" placeholder="type something here.." /> */}
+              <button className="filter-btn" onClick={ ()=>{
+                const filterList = listRes.data.filter((res)=> res.avg_rating > 4)
+                SetListRes({...listRes, data: filterList})
+              }}>Top Rated Restaurant</button>
             </div>
 
             <div className="res-container">
-  
-               {resObj.data.map((restaurant) => (
+
+               {listRes.data.map((restaurant) => (
                    <RestaurantCard key={restaurant.id} resData={restaurant} />
                ))}
 
