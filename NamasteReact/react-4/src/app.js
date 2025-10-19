2629d2a64215a0811bc547b09c250107162dbd19
Shubham kahar <Shubhamkahar196@gmail.com>
2025-10-19 11:37:59 +0530

    react- 5 and understanding import and export

--- NamasteReact/react-4/src/app.js
+++ NamasteReact/react-4/src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-
+import Header from "./components/Header";
 /*
 header
 -logo 
@@ -20,24 +20,6 @@ body
 -contact
 */
 
-const Header = () =>{
-    return (
-        <div className="header">
-         <div className="logo-container">
-         <img  className="logo" src="https://img.freepik.com/premium-vector/food-delivery-service-logo-design-icon-vector-template_1157159-94.jpg?semt=ais_hybrid&w=740&q=80" alt="logo"/>
-         </div>
-
-         <div className="nav-items">
-   <ul>
-    <li>Home</li>
-    <li>About Us</li>
-    <li>Contact Us</li>
-    <li>Cart</li>
-   </ul>
-         </div>
-        </div>
-    )
-}
 
 
 const resObj = {
---
+++ NamasteReact/react-4/src/components/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () =>{
+    return (
+        <div className="header">
+         <div className="logo-container">
+         <img  className="logo" src="https://img.freepik.com/premium-vector/food-delivery-service-logo-design-icon-vector-template_1157159-94.jpg?semt=ais_hybrid&w=740&q=80" alt="logo"/>
+         </div>
+
+         <div className="nav-items">
+   <ul>
+    <li>Home</li>
+    <li>About Us</li>
+    <li>Contact Us</li>
+    <li>Cart</li>
+   </ul>
+         </div>
+        </div>
+    )
+}
+
+export default Header
