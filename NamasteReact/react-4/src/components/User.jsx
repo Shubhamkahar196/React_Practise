1ec95d11d408c3b9acec7a18081e83138519e97c
Shubham kahar <Shubhamkahar196@gmail.com>
2025-12-14 09:00:06 +0530

    Understanding useState in class and functional components

--- NamasteReact/react-4/src/components/User.jsx
+++ NamasteReact/react-4/src/components/User.jsx
@@ -1,8 +1,19 @@
+import {useState} from 'react'
 
-const User = (props)=>{
+// const User = (props)=>{
+const User = ({name})=>{
+    const [count,setCount] = useState(0);
+    // const [count2] = useState(2);
     return (
         <div className="user-card">
-         <h2>Name: {props.name}</h2>
+            <h1>Count: {count}</h1>
+            <button onClick={()=>{
+                setCount(count+1);
+            }}>Count Increase</button>
+
+
+         {/* <h2>Name: {props.name}</h2> */}
+         <h2>Name: {name}</h2>
          <h3>Location: Varanasi</h3>
          <h4>Contact: @shubham</h4>
         </div>
--- NamasteReact/react-4/src/components/UserClass.jsx
+++ NamasteReact/react-4/src/components/UserClass.jsx
@@ -6,12 +6,26 @@ class UserClass extends React.Component{
         // this is important
         super(props);
 
-        console.log(props)
+        // use state in class components
+        this.state ={
+            count:0,
+            count2:2
+        }
     }
 
     render(){
+        // destructuring
+        const {count} = this.state;
         return (
         <div className="user-card">
+            <h1>Count: {count}</h1>
+          <button onClick={()=>{
+            // never update state variables directly
+
+            this.setState({
+                count: this.state.count+1,
+            })
+          }}>Count Increase</button>
          <h2>Name: {this.props.name}</h2>
          <h3>Location: Varanasi</h3>
          <h4>Contact: @shubham</h4>
