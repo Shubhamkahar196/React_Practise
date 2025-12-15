5606e3e87f3d4eeaf13d7c166292302d21f1b6c0
Shubham kahar <Shubhamkahar196@gmail.com>
2025-12-15 06:47:47 +0530

    understanding api call with componentDidMount

--- NamasteReact/react-4/src/components/AboutUs.jsx
+++ NamasteReact/react-4/src/components/AboutUs.jsx
@@ -6,22 +6,23 @@ class AboutUs extends React.Component{
   constructor(props){
     super(props);
      
-    console.log("Parent class")
+    // console.log("Parent class")
     
   }
   //  componentDidMount is use for api call just like useEffect
   componentDidMount(){
-    console.log("Parent mount")
+    // console.log("Parent mount")
   }
 
   render(){
-    console.log("parent render")
+    // console.log("parent render")
     return (
     <div>
         <h1>About</h1>
         <h2>Learing About us</h2>
         <User name={"Shubham(functional"}/>
         <UserClass name={"Shubham(class)"}/>
+
     </div>
   )
   }
--- NamasteReact/react-4/src/components/UserClass.jsx
+++ NamasteReact/react-4/src/components/UserClass.jsx
@@ -7,36 +7,64 @@ class UserClass extends React.Component{
         super(props);
 
         // use state in class components
-        this.state ={
-            count:0,
-            count2:2
+        // this.state ={
+        //     count:0,
+        //     count2:2
+        // }
+        this.state = {
+            userInfo: "Dummay",
+            location: "Default",
+            avatar_url: "default"
         }
-      console.log("Child constructor")
-    }
+          
+    } 
+//  api call ke liye
+   async componentDidMount(){
+       const data = await fetch("https://api.github.com/users/shubhamkahar196");
+       const json = await data.json();
 
-    componentDidMount(){
-        console.log("Child mount")
+
+       this.setState({
+        userInfo: json,
+       })
+       console.log(json);
     }
 
     render(){
 
-        console.log("Child render")
+        // console.log("Child render")
         // destructuring
-        const {count} = this.state;
-        return (
-        <div className="user-card">
-            <h1>Count: {count}</h1>
-          <button onClick={()=>{
+        // const {count} = this.state;
+
+        // return (
+        // <div className="user-card">
+        //     <h1>Count: {count}</h1>
+        //   <button onClick={()=>{
             // never update state variables directly
 
-            this.setState({
-                count: this.state.count+1,
-            })
-          }}>Count Increase</button>
-         <h2>Name: {this.props.name}</h2>
-         <h3>Location: Varanasi</h3>
-         <h4>Contact: @shubham</h4>
-        </div>
+        //     this.setState({
+        //         count: this.state.count+1,
+        //     })
+        //   }}>Count Increase</button>
+
+
+    //      <h2>Name: {name}</h2>
+    //      <h3>Location: Varanasi</h3>
+    //      <h4>Contact: @shubham</h4>
+    //     </div>
+    // )
+
+        const {name,location,avatar_url} = this.state.userInfo;
+    return (
+
+
+        <div className="user-card">
+            <img src={avatar_url} alt="" srcset="" />
+<h2>Name: {name}</h2>
+
+<h3> Location: {location}</h3>
+<h3> Contact: shubhame</h3>
+     </div>
     )
     }
 }
